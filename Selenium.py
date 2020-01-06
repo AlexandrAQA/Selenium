490802c28a6f9741bec33b2f9d0d7931e9d324db
AndroidPanaev <panaev.as@gmail.com>
2020-01-06 16:26:12 +0300

    find_element_by_xpath

--- Selenium.py
+++ Selenium.py
@@ -1,18 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 webdriver = webdriver.Chrome(executable_path= "C:\Drivers\chromedriver.exe")
 #webdriver = webdriver.Firefox( executable_path= "C:\Drivers\geckodriver.exe")
 #webdriver = webdriver.Ie(executable_path= "C:\Drivers\IEDriverServer")
-
-
 #webdriver.get("https://www.amazon.com/")
+
+
 webdriver.get("http://demo.automationtesting.in/Windows.html")
 
-print(webdriver.title)
+print(webdriver.title) #returns the title of the page
+print(webdriver.current_url) #returns URL of the page
 
-print(webdriver.current_url)
+webdriver.find_element_by_xpath('//*[@id="Tabbed"]/a/button').click()
+
+time.sleep(5)
+
 
 #print(webdriver.page_source) #HTML code for the page (huge information)
-print("Finish of the First part")
-webdriver.close()
+
+
+#webdriver.close()  #closes only currently focussed browser tab
+webdriver.quit()   #closes all tabs of the browser
